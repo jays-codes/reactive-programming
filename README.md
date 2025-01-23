421f916445cb10058472f9de30e6ef3bc2e851da
jay menorca <zaimenorca@gmail.com>
2025-01-23 10:18:33 -0500

    defined request/cancel logic in SubscriptionImpl

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Jay's proj repo for Reactive Programming in Java
 
 proj: reactive-sandbox
+- defined request/cancel logic in SubscriptionImpl
 - added: Subscriber, Publisher, Subscription impls
 - created. dependencies: Java 21, Reactor, Logback, JUnit
--- reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/publisher/SubscriptionImpl.java
+++ reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/publisher/SubscriptionImpl.java
@@ -5,23 +5,41 @@ import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.javafaker.Faker;
 public class SubscriptionImpl implements Subscription {
-    private static final Logger logger = LoggerFactory.getLogger(SubscriptionImpl.class);
 
-    private Subscriber<? super String> subscriber;
+    private static final Logger logger = LoggerFactory.getLogger(SubscriptionImpl.class);
+    private static final int MAX_ITEMS = 10;
+    private final Faker faker;
+    private final Subscriber<? super String> subscriber;
     private boolean isCancelled = false;
-    
+    private int count = 0;
+
     public SubscriptionImpl(Subscriber<? super String> subscriber) {
         this.subscriber = subscriber;
+        this.faker = Faker.instance();
     }
 
     @Override
-    public void request(long n) {
-        logger.info("requesting {}", n);
+    public void request(long requested) {
+        if (isCancelled) {
+            return;
+        }
+        logger.info("subscriber requested {} items", requested);
+        for (int i = 0; i < requested && count < MAX_ITEMS; i++) {
+            count++;
+            this.subscriber.onNext(this.faker.internet().emailAddress());
+        }
+        if (count == MAX_ITEMS) {
+            logger.info("no more data to produce");
+            this.subscriber.onComplete();
+            this.isCancelled = true;
+        }
     }
 
     @Override
     public void cancel() {
         logger.info("subscriber has cancelled");
+        this.isCancelled = true;
     }
 }
