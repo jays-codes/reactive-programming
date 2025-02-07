e4348f011fc6d2c3b307eba20474dab0242d19f5
jay menorca <zaimenorca@gmail.com>
2025-02-07 09:49:15 -0500

    created FluxEmptyError: empty(), error()

---
+++ reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/FluxEmptyError.java
@@ -0,0 +1,21 @@
+package jayslabs.reactive.sandbox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jayslabs.reactive.sandbox.common.Util;
+import reactor.core.publisher.Flux;
+
+public class FluxEmptyError {
+    private static final Logger log = LoggerFactory.getLogger(FluxEmptyError.class);
+
+    public static void main(String[] args) {
+
+        Flux.empty()
+        .subscribe(Util.subscriber());
+
+        Flux.error(new RuntimeException("Exception"))
+        .subscribe(Util.subscriber());
+
+    }
+}
