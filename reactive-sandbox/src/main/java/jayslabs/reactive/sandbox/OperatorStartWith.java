ccd69421612c5e101fc11c5772d6abae43fc2cc8
jay menorca <zaimenorca@gmail.com>
2025-02-24 15:40:27 -0500

    OperatorStartWith: added nameGeneratorWithCacheDemo() and modified NameGenrator to add method returning cache-enabled publisher to show publisher caching with startWith()

--- reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/OperatorStartWith.java
+++ reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/OperatorStartWith.java
@@ -4,14 +4,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jayslabs.reactive.sandbox.common.Util;
+import jayslabs.reactive.sandbox.helper.NameGenerator;
 import reactor.core.publisher.Flux;
 
 public class OperatorStartWith {
     private static final Logger log = LoggerFactory.getLogger(OperatorStartWith.class);
 
     public static void main(String[] args) {
-        demo1();
-        demo2();
+        //demo1();
+        //demo2();
+        //nameGeneratorDemo();
+        nameGeneratorWithCacheDemo();
+    }
+
+    public static void nameGeneratorDemo(){
+        var names = new NameGenerator();
+        names.generateNamesFluxWithDelay().take(3).subscribe(Util.subscriber("anya"));
+        names.generateNamesFluxWithDelay().take(3).subscribe(Util.subscriber("becky"));
+        names.generateNamesFluxWithDelay().take(3).subscribe(Util.subscriber("bondo"));
+    }
+
+    public static void nameGeneratorWithCacheDemo(){
+
+        var names = new NameGenerator();
+        names.generateNamesFluxWithCache().take(3).subscribe(Util.subscriber("anya"));
+        names.generateNamesFluxWithCache().take(3).subscribe(Util.subscriber("becky"));
+        names.generateNamesFluxWithCache().take(3).subscribe(Util.subscriber("bondo"));
+
+
     }
 
     public static void demo1() {
--- reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/helper/NameGenerator.java
+++ reactive-sandbox/src/main/java/jayslabs/reactive/sandbox/helper/NameGenerator.java
@@ -1,5 +1,6 @@
 package jayslabs.reactive.sandbox.helper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
@@ -10,11 +11,13 @@ import org.slf4j.LoggerFactory;
 import jayslabs.reactive.sandbox.common.Util;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
-
 public class NameGenerator implements Consumer<FluxSink<String>>{
 
     private FluxSink<String> fluxSink;
     private static final Logger log = LoggerFactory.getLogger(NameGenerator.class);
+    
+    private static final List<String> namesCache = new ArrayList<>();
+
     @Override
     public void accept(FluxSink<String> fluxSink) {
         log.info("accept() method called");
@@ -38,11 +41,28 @@ public class NameGenerator implements Consumer<FluxSink<String>>{
             .map(i -> generateName());
     }
 
-    
+    public static final Flux<String> generateNamesFluxWithDelay(){
+        return Flux.generate(sink -> {
+            log.info("Generating name");
+            Util.sleepSeconds(1);
+            var name = Util.faker().name().firstName();
+            sink.next(name);
+        });
+    }
 
+    public static final Flux<String> generateNamesFluxWithCache(){
+        return Flux.generate(sink -> {
+            log.info("Generating name");
+            Util.sleepSeconds(1);
 
-    //simulating a resource intensive operation (takes 1 second to execute)
-    private static String generateName() {
+            var name = Util.faker().name().firstName();
+            namesCache.add(name);
+            sink.next(name);
+        }).startWith(namesCache).cast(String.class);
+    }
+
+    //generateName()
+    public static String generateName(){
         Util.sleepSeconds(1);
         return Util.faker().name().firstName();
     }
